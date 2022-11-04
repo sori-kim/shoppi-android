0a5cf850e4d29b9bb0330f520c23cb590931bf9e
Rosie Kim <rosie@medility.ai>
2022-11-04 14:09:51 +0900

    feat: webview 삽입 및 JS 통신코드 작성

---
+++ app/src/main/java/com/shoppi/app/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.shoppi.app
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.JavascriptInterface
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import android.widget.Toast
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView = findViewById<WebView>(R.id.webView)
+
+        webView.apply {
+            webViewClient = WebViewClient()
+            webChromeClient = WebChromeClient()
+            webView.settings.javaScriptEnabled = true // 자바스크립트 허용
+        }
+
+        webView.addJavascriptInterface(WebAppInterface(this), "PilleyeBridge")
+
+        webView.loadUrl("https://medility-fe-private-mobile.vercel.app")
+
+    }
+
+    class WebAppInterface(private val mContext: Context){
+        @JavascriptInterface
+
+        fun showToast(toast: String){
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+
+
+    // 백버튼을 클릭했을때
+    override fun onBackPressed() {
+        super.onBackPressed()
+        val webView = findViewById<WebView>(R.id.webView)
+        // 뒤로 갈 페이지가 존재한다면
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed() // 안드로이드의 백버튼 기능 수행
+        }
+    }
+
+}
+
+
+
